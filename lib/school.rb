fb9a0346aeee106d8ef7a8a0cab3ee01a86bfc03
kennja05 <jacobkenny05@gmail.com>
2019-12-10 15:07:25 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,29 +2,34 @@
 require 'pry'
 
 class School
-
-      attr_accessor :roster, :name,  
-    
+    attr_accessor :name, :roster
+  
     def initialize(name)
-        @name = name
-        @roster = {}
-    end 
-
-    def roster
-        @roster = {}
-    end 
-
-    def roster=(roster)
-        @roster = roster
-    end 
-
+      @name = name
+      @roster = {}
+    end
+  
     def add_student(student, grade)
-       #binding.pry 
-        if !roster[grade]
-            roster[grade] = []
-            roster[grade] << student
-        else 
-            roster[grade] << student 
+      if !roster[grade]
+        roster[grade] = []
+        roster[grade] << student 
+      else 
+      roster[grade] << student
+      end
+    end 
+
+    def grade(grade)
+        roster[grade]
+    end 
+
+    def sort
+        my_hash = {}
+        roster.each do |grade, student_array|
+           # binding.pry 
+            my_hash[grade] = student_array.sort
+
         end 
-    
+        my_hash 
+    end 
+
 end 
