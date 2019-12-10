7aaf3a98de584340b05f360d113e7752ff0da0a9
Calvin Baker <calvin.baker128@gmail.com>
2019-12-10 19:02:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  def initialize(school_name, roster={})
+    @roster = roster
+    @school_name = school_name
+  end
+  
+  attr_reader :school_name, :roster
+  
+  def add_student(name, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(year)
+    @roster[year]
+  end
+  
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade].sort!
+    end
+  end
+end
